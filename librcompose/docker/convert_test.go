1c1144812a6e46cace807af527d41520e2cf6be7
Ivan Mikushin <i.mikushin@gmail.com>
2015-05-18 14:49:55 +0500

    use testify to cleanup test

--- librcompose/docker/convert_test.go
+++ librcompose/docker/convert_test.go
@@ -1,19 +1,14 @@
 package docker
 
 import (
-	"reflect"
 	"testing"
+	"github.com/stretchr/testify/assert"
 	shlex "github.com/flynn/go-shlex"
 )
 
 func TestParseCommand(t *testing.T) {
 	exp := []string{"sh", "-c", "exec /opt/bin/flanneld -logtostderr=true -iface=${NODE_IP}"}
 	cmd, err := shlex.Split("sh -c 'exec /opt/bin/flanneld -logtostderr=true -iface=${NODE_IP}'")
-	switch {
-	case err != nil:
-		t.Errorf("Error: %v\n", err)
-	case !reflect.DeepEqual(cmd, exp):
-		t.Errorf("got: %v\n", cmd)
-		t.Errorf("expected: %v\n", exp)
-	}
+	assert.Nil(t, err)
+	assert.Equal(t, exp, cmd)
 }

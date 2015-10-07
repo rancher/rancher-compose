750ab00861ce30c6e98417a74a764d7b7f263b82
Darren Shepherd <darren@rancher.com>
2015-10-07 00:15:10 -0700

    Make error message more friendly when API keys are wrong
    https://github.com/rancher/rancher/issues/2123

--- rancher/util.go
+++ rancher/util.go
@@ -1,5 +1,14 @@
 package rancher
 
+func Contains(list []string, item string) bool {
+	for _, v := range list {
+		if v == item {
+			return true
+		}
+	}
+	return false
+}
+
 func ToMapInterface(data map[string]string) map[string]interface{} {
 	ret := map[string]interface{}{}
 

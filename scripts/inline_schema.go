e14c7c8afaae9b234cfff3af43dac84f439af9b4
Josh Curl <hello@joshcurl.com>
2016-03-19 22:24:17 -0700

    Strict yaml validation

---
+++ scripts/inline_schema.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io/ioutil"
+	"os"
+	"text/template"
+)
+
+func main() {
+	t, err := template.New("schema_template.go").Parse(`
+package project
+
+var schemaString = ` + "`{{.schemaString}}`")
+
+	if err != nil {
+		panic(err)
+	}
+
+	schema, err := ioutil.ReadFile("./scripts/config_schema_v1.json")
+
+	inlinedFile, err := os.Create("vendor/github.com/docker/libcompose/project/schema.go")
+
+	if err != nil {
+		panic(err)
+	}
+
+	err = t.Execute(inlinedFile, map[string]string{
+		"schemaString": string(schema),
+	})
+
+	if err != nil {
+		panic(err)
+	}
+}

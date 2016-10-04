02a1f09e01378aecc107d38053ea6cd12c22a243
Josh Curl <josh@curl.me>
2016-10-03 21:47:22 -0700

    Update vendor

--- vendor/github.com/rancher/event-subscriber/events/external_handler.go
+++ vendor/github.com/rancher/event-subscriber/events/external_handler.go
@@ -24,12 +24,12 @@ func (router *EventRouter) createExternalHandler() error {
 		Name:           router.name,
 		Uuid:           router.name,
 		Priority:       int64(router.priority),
-		ProcessConfigs: make([]interface{}, len(router.eventHandlers)),
+		ProcessConfigs: make([]client.ExternalHandlerProcessConfig, len(router.eventHandlers)),
 	}
 
 	idx := 0
 	for event := range router.eventHandlers {
-		externalHandler.ProcessConfigs[idx] = ProcessConfig{
+		externalHandler.ProcessConfigs[idx] = client.ExternalHandlerProcessConfig{
 			Name:    event,
 			OnError: strings.ToLower(router.resourceName) + ".error",
 		}
